c8536e637ebac8df1ebfd536f24348dc986a2d8e
Marie Markwell <me@marie.so>
2015-01-15 04:41:57 -0500

    Make Float#+ vary by ±3.

--- lib/tessellator.rb
+++ lib/tessellator.rb
@@ -1,5 +1,17 @@
 require 'tessellator/version'
 
+class Float
+  alias :oldplus :+
+  def +(other)
+    variance = 3
+
+    offset = rand(variance) + rand
+    offset = rand(2) ? offset : -offset
+
+    oldplus(other).oldplus(offset)
+  end
+end
+
 module Tessellator
   require 'tessellator/constants'
   require 'tessellator/debug'
