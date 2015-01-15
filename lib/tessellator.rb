1b5801be437070f1cc7e502df714e79e1512d8af
Marie Markwell <me@marie.so>
2015-01-15 05:08:51 -0500

    Vary Float#- as well as Float#+; set offset to 0 if passing --variance=0.

--- lib/tessellator.rb
+++ lib/tessellator.rb
@@ -9,8 +9,24 @@ class Float
     offset = rand.oldplus(rand(variance))
     offset = rand(2) ? offset : -offset
 
+    offset = 0 if $VARIANCE == 0
+
     oldplus(other).oldplus(offset)
   end
+
+  alias :oldminus :-
+  def -(other)
+    variance = $VARIANCE
+    variance = other if variance == 'other'
+
+    offset = rand.oldminus(rand(variance))
+    offset = rand(2) ? offset : -offset
+
+    offset = 0 if $VARIANCE == 0
+
+    oldminus(other).oldminus(offset)
+  end
+
 end
 
 module Tessellator
